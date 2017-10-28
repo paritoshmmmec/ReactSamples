1d142e14e79dcdad30878d5a87fbc1392b5377da
paritosh baghel <paritoshmmmec@gmail.com>
2017-10-28 18:28:30 -0400

    React start
    React start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import ListItem from './ListItem';
 import Welcome from './Welcome';
+import Star from './component/Star';
 
 export default class App extends Component {
   constructor() {
     super()
     this.persons = [{ Id: 1, firstName: "Paritosh" },
     { Id: 2, firstName: "Service" }];
+    this.state = {
+      counter: 1
+    }
   }
 
   myFunction(x, y, z) {
-    console.log(x);
-    console.log(y);
-    console.log(z);
+  }
+
+  activateLasers = () => {
+    this.setState(prevState => (
+      {
+        counter: prevState.counter + 1
+      }
+    ));
+   
   }
 
   render() {
@@ -26,8 +36,12 @@ export default class App extends Component {
 
     return (
       <div>
-        <Welcome name="paritosh"/>
+        <Welcome name="paritosh" />
         <ul>{element}</ul>
+        <Star />
+        <button onClick={this.activateLasers}>
+          Activate Lasers
+        </button>
       </div>
     );
   }
--- my-app/src/component/Star.js
+++ my-app/src/component/Star.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 export default class Star extends Component {
-
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
+        this.state = {
+            date: new Date()
+        };
     }
 
-    
+    componentDidMount() {
+        console.log('iam getting called');
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
 
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.date.toString()}</h1>
+            </div>
+        );
+    }
 }
